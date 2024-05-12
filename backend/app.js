7aa1f7710fe01d7280a83f774c31f20d7e217f70
medb2m <79202818+medb2m@users.noreply.github.com>
2024-05-12 03:03:33 +0100

    added CORS and express.json()

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
 import express  from "express"
 import morgan from "morgan"
+import cors from 'cors'
 
 const app=express()
 
+
+app.use(cors())
+app.use(express.json())
 app.use(morgan("dev"))
+
+
 app.use((req, res, next) => {
     console.log('First MiddleWare')
     next()
--- server.js
+++ server.js
@@ -6,5 +6,5 @@ app.set('port', port)
 
 
 app.listen(port, ()=>{ 
-    console.log('Server running..')
+    console.log(`Server running on http://localhost:${port}`)
 })
