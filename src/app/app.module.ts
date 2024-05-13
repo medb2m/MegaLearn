12b331f04521b20dd811ddf1da97379533ea5ae3
medb2m <79202818+medb2m@users.noreply.github.com>
2024-05-13 09:29:16 +0100

    added http interceptor & updated app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardTutorComponent } from './board-tutor/board-tutor.component';
+
+import { httpInterceptorProviders } from './_helpers/http.interceptor';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardUserComponent,
+    BoardTutorComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +34,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule,
     FormsModule
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
