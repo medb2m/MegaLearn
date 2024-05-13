1d857db172840775648b7dcaa5853e71a49f28ec
medb2m <79202818+medb2m@users.noreply.github.com>
2024-05-13 07:15:49 +0100

    defined endpoints

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,25 @@
+import { checkDuplicateUsernameOrEmail, checkRolesExisted } from "../middlewares/verifySignUp.js";
+import { signup, signin, signout } from "../controllers/auth.controller.js";
+
+export default function authRoutes(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/api/auth/signup",
+    [
+      checkDuplicateUsernameOrEmail,
+      checkRolesExisted
+    ],
+    signup
+  );
+
+  app.post("/api/auth/signin", signin);
+
+  app.post("/api/auth/signout", signout);
+};
