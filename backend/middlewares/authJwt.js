46dfa6de7ab5e7e6728771527c36a5d24717495d
medb2m <79202818+medb2m@users.noreply.github.com>
2024-05-13 08:14:19 +0100

    updated authJwt.js to catch TokenExpiredError in VerifyToken()

--- backend/middlewares/authJwt.js
+++ backend/middlewares/authJwt.js
@@ -3,8 +3,36 @@ import { authConfig } from "../config/auth.config.js";
 import db from '../models/users/index.js';
 
 const { user: User, role: Role } = db;
+const { TokenExpiredError } = jwt
 
-export const verifyToken = async (req, res, next) => {
+const catchError = (err, res) => {
+  if (err instanceof TokenExpiredError) {
+    return res.status(401).send({ message: "Unauthorized! Access Token was expired!" });
+  }
+
+  return res.status(401).send({ message: "Unauthorized!" });
+}
+// with token refresh
+
+export const verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) {
+      return catchError(err, res);
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+
+// with Token refresh 
+/* export const verifyToken = async (req, res, next) => {
   try {
     const token = req.session.token;
 
@@ -18,7 +46,7 @@ export const verifyToken = async (req, res, next) => {
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized!" });
   }
-};
+}; */
 
 const checkUserRole = async (req, res, next, roleName) => {
   try {
