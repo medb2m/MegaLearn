d408fd96e1ebc57473b832db2b25f9b5ff369892
medb2m <79202818+medb2m@users.noreply.github.com>
2024-05-20 01:30:31 +0100

    Added authenticate function to user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-﻿import Joi from 'joi';
+import Joi from 'joi';
 import validateRequest from '../_middleware/validate-request.js'
 import UserService from './user.service.js'
 import Role from '../_helpers/role.js';
@@ -12,3 +12,13 @@ export function authenticateSchema(req, res, next) {
     validateRequest(req, next, schema);
 }
 
+export function authenticate(req, res, next) {
+    const { email, password } = req.body;
+    const ipAddress = req.ip;
+    UserService.authenticate({ email, password, ipAddress })
+        .then(({ refreshToken, ...user }) => {
+            setTokenCookie(res, refreshToken);
+            res.json(user);
+        })
+        .catch(next);
+}
