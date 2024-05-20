fe3fbd63ebc6ab5ac63b720776164ae175ed85cc
medb2m <79202818+medb2m@users.noreply.github.com>
2024-05-20 01:31:32 +0100

    Added revokeToken & refreshToken functions to user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -22,3 +22,38 @@ export function authenticate(req, res, next) {
         })
         .catch(next);
 }
+
+export function refreshToken(req, res, next) {
+    const token = req.cookies.refreshToken;
+    const ipAddress = req.ip;
+    UserService.refreshToken({ token, ipAddress })
+        .then(({ refreshToken, ...user }) => {
+            setTokenCookie(res, refreshToken);
+            res.json(user);
+        })
+        .catch(next);
+}
+
+export function revokeTokenSchema(req, res, next) {
+    const schema = Joi.object({
+        token: Joi.string().empty('')
+    });
+    validateRequest(req, next, schema);
+}
+
+export function revokeToken(req, res, next) {
+    // accept token from request body or cookie
+    const token = req.body.token || req.cookies.refreshToken;
+    const ipAddress = req.ip;
+
+    if (!token) return res.status(400).json({ message: 'Token is required' });
+
+    // users can revoke their own tokens and admins can revoke any tokens
+    if (!req.user.ownsToken(token) && req.user.role !== Role.Admin) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    UserService.revokeToken({ token, ipAddress })
+        .then(() => res.json({ message: 'Token revoked' }))
+        .catch(next);
+}
