6eba8cea08d380471684c43fdff1f34a5182b648
Majdi Zlitni <majdiuchiha@gmail.com>
2024-05-23 22:12:41 +0100

    adding course model

---
+++ backend/models/cours.model.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const CourseSchema = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  creator: { type: Schema.Types.ObjectId, ref: 'User' },
+  videos: [{ type: Schema.Types.ObjectId, ref: 'Video' }],
+  enrolls: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+  //TODO Add categorie 
+});
+
+export default model('Course', CourseSchema);
