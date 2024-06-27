ec18dbf153d18c5a47fe125b3a9bd850e9ef0529
medb2m <79202818+medb2m@users.noreply.github.com>
2024-06-27 21:39:59 +0100

    update emailing feature

--- backend/_helpers/config.js
+++ backend/_helpers/config.js
@@ -1,6 +1,6 @@
 export const config = {
-    connectionString: "mongodb://127.0.0.1:27017/finalPI",
-    //connectionString: "mongodb+srv://medb2m:theHolyPass2121.@cluster0.yziclg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    //connectionString: "mongodb://127.0.0.1:27017/finalPI",
+    connectionString: "mongodb+srv://medb2m:theHolyPass2121.@cluster0.yziclg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     secret: "Perpill secret 361!",
     /* FACE_API_KEY: '96f1ace58c424d06998f93bc9d98c2f3',
     FACE_API_HOST: 'https://perpill.cognitiveservices.azure.com/',
@@ -9,11 +9,12 @@ export const config = {
     FACE_API_CONFIDENCE_TRESHOLD: 0.75, */
     emailFrom: "medobouh@gmail.com",
     smtpOptions: {
-        host: "smtp.ethereal.email",
+        service : "gmail",
+        host: "smtp.gmail.com",
         port: 587,
         auth: {
-            user: "karson.fisher64@ethereal.email",
-            pass: "JYwU8gKF7muGZ6jNkE"
+            user: "medobouh@gmail.com",
+            pass: "dgah djgd zqng rtnh"
         }
     }
 }
