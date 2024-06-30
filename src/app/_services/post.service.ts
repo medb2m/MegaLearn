3f84d50b0a7eebc299755a1a0bd6faa33cfcefd4
medb2m <79202818+medb2m@users.noreply.github.com>
2024-06-30 04:10:04 +0100

    create blog service

---
+++ src/app/_services/post.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '@environments/environment';
+import { Comment } from '@app/_models/comment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private apiUrl = `${environment.apiUrl}/blog/posts`;
+
+  constructor(private http: HttpClient) { }
+
+  getAllPosts(): Observable<any> {
+    return this.http.get(`${this.apiUrl}`);
+  }
+
+  getPostById(id: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  createPost(post: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}`, post);
+  }
+
+  updatePost(id: string, post: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, post);
+  }
+
+  deletePost(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  createComment(postId: string, comment : any) {
+    return this.http.post(`${this.apiUrl}/comments/${postId}`, comment);
+  }
+
+  getAllComments(postId : string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/comments/${postId}`);
+  }
+}
+
+
+
+/* router.get('/comments/:id', getCommentById);
+router.put('/comments/:id', authorize(), updateCommentById);
+router.delete('/comments/:id', authorize(), deleteCommentById); */
